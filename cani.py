6ad8cddfb1043d01f1373f30a298e8584c35e6f4
Eandrea76 <andelia35@yahoo.it>
2024-12-05 14:52:52 +0100

    creazione nuovo dataset

--- cani.py
+++ cani.py
@@ -9,27 +9,59 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler 
+
+##data_set_path="emozione_cane"
+data_set_path="images"
+#resize_dataset_dog(data_set_path,size=(32,32))
+
+# provo un metodo pre risparmiare memoria
+# per converitire i numere in float32
+
+# data_set = [image.flatten() for image in data_set_path]
+# data_set = np.array(data_set, dtype=np.float32)
 
 
-data_set_path="emozione_cane"
-resize_dataset_dog(data_set_path,size=(400,400))
+data_set, labels=create_dataset_dog(data_set_path)
+# # # print(data_set[])
+# # # print(labels[])
+# # pca = PCA(n_components=100) 
+# # data_set = pca.fit_transform(data_set)
 
-data_set, labels=create_dataset_dog("emozione_cane_resized")
-# print(data_set[])
-# print(labels[])
-pca = PCA(n_components=100) 
+
+# appiattimento della funzione
+data_set = np.array([image.flatten() for image in data_set], dtype=np.float32)
+
+
+#normaliziamo i dati
+scaler = StandardScaler()
+data_set = scaler.fit_transform(data_set)
+
+# usiamo la pca con un numero piu basso
+pca = PCA(n_components=50)  
 data_set = pca.fit_transform(data_set)
 
 
-dataset_norm = [(image - image.min()) / (image.max() - image.min()) for image in data_set]
-print(dataset_norm[0])
 
+
+
+
+# # dataset_norm = [(image - image.min()) / (image.max() - image.min()) for image in data_set]
+# # print(dataset_norm[0])
+
+# Codifica delle etichette
 label_encoder = LabelEncoder()
-label_encoder.fit(labels)
-labels = label_encoder.transform(labels)
+labels = label_encoder.fit_transform(labels)
+
+
+
+#codificazione delle etichette
+# label_encoder = LabelEncoder()
+# label_encoder.fit(labels)
+##labels = label_encoder.transform(labels)
 #print(labels)
 
-X_train, X_test, y_train, y_test = train_test_split(dataset_norm, labels, random_state=30, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(data_set, labels, random_state=30, test_size = 0.2)
 
 
 # prova con svc
